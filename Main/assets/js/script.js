bc8edc2666f2c7f81850199b113d8e0e0c65ca38
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-21 22:40:04 -0400

    set the default value

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -1,10 +1,10 @@
 var highScoreLink = document.querySelector("#highScores");
 var submitButton = document.querySelector("#submit-button");
 var startQuizButton = document.querySelector(".start-quiz");
-var timerCount = 5;
-var timeLeft = 0;
+var timerClock = document.querySelector("#timer");
+var timerCount = 60;
+timerClock.innerHTML = timerCount; //default value of the timer
 var idTimer = null;
-
 var highScores = []
 
 function startTimer() {
@@ -13,7 +13,6 @@ function startTimer() {
 
 function updateTimerDisplay() {
     timerCount--
-    var timerClock = document.querySelector("#timer");
     timerClock.innerHTML = timerCount;
     if (timerCount === 0) {
         clearInterval(idTimer);
