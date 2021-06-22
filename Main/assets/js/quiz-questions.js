25fbdd824d93f202784c17a447c47363a5e30c14
Needa Mehmud <needa.mehmud@gmail.com>
2021-06-21 22:42:16 -0400

    set the default value

---
+++ Main/assets/js/quiz-questions.js
@@ -0,0 +1,12 @@
+var questionsArray = [
+    {
+        title: "String values must be enclosed within ___ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "parethesis"],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool during development and debugging for printing content to the debugger is: ",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
+        answer: "terminal/bash"
+    },
+];
