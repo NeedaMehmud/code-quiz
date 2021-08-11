a6de96fa71161c1a6a361a5fe564352d59b4fd75
Needa Mehmud <needa.mehmud@gmail.com>
2021-08-11 10:56:47 -0400

    refactoring code - changed variable names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,17 @@
-var highScoreLink = document.querySelector("#highScores");
-var submitButton = document.querySelector("#submit-button");
 var startQuizButton = document.querySelector("#start-quiz");
 var timerClock = document.querySelector("#timer");
-var resultsContainer = document.querySelector('#results');
+var question = document.querySelector("#question");
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+var quizSection = document.querySelector("#quiz-section");
+var individualResult = document.querySelector("#individual-results");
+var initialsSection = document.querySelector("#initials-section");
+var submitButton = document.querySelector("#submit-button");
+var highscoreSection = document.querySelector("#highscores-section");
+var viewHighscores = document.querySelector("#highscore");
+
 
 var timerCount = 75;
 //default value of the timer
